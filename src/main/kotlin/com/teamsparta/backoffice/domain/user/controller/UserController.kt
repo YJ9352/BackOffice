5d46aa612ba4d55e07596f97be1b91c10c659738
오재영 <jeayeong70@gmail.com>
2024-01-23 22:40:00 +0900

    Feat : "인가 : 본인 정보는 본인만 열람 가능"

--- src/main/kotlin/com/teamsparta/backoffice/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/teamsparta/backoffice/domain/user/controller/UserController.kt
@@ -2,8 +2,11 @@ package com.teamsparta.backoffice.domain.user.controller
 
 import com.teamsparta.backoffice.domain.user.dto.*
 import com.teamsparta.backoffice.domain.user.service.UserService
+import com.teamsparta.backoffice.infra.security.jwt.UserPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,12 +26,21 @@ class UserController(
     }
 
     @GetMapping("/{id}")
-    fun searchMyInfo(@PathVariable id: Long) : ResponseEntity<SearchUserResponse> {
+    @PreAuthorize("#userPrincipal.id == #id")
+    fun searchMyInfo(
+            @PathVariable id: Long,
+            @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ) : ResponseEntity<SearchUserResponse> {
         return ResponseEntity.status(HttpStatus.OK).body(userService.searchMyInfo(id))
 
     }
     @PatchMapping("/{id}")
-    fun modifyMyInfo(@PathVariable id: Long, @RequestBody modifyUserRequest: ModifyUserRequest) : ResponseEntity<SearchUserResponse> {
+    @PreAuthorize("#userPrincipal.id == #id")
+    fun modifyMyInfo(@PathVariable id: Long,
+                     @RequestBody modifyUserRequest: ModifyUserRequest,
+                     @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ) : ResponseEntity<SearchUserResponse> {
+
         return ResponseEntity.status(HttpStatus.OK).body(userService.modifyMyInfo(id,modifyUserRequest))
 
     }
