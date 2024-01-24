9a94a10bbe69fd89942be620291882f4567ff8f0
seunghwan <lovelyunsh@naver.com>
2024-01-24 11:14:51 +0900

    Style : AddCartMenuRequest forceInsert 프로퍼티 제거

--- src/main/kotlin/com/teamsparta/backoffice/domain/cart/dto/AddCartMenuRequest.kt
+++ src/main/kotlin/com/teamsparta/backoffice/domain/cart/dto/AddCartMenuRequest.kt
@@ -3,6 +3,5 @@ package com.teamsparta.backoffice.domain.cart.dto
 data class AddCartMenuRequest(
     val storeId: Long,
     val menuId: Long,
-    val count: Int,
-    val forceInsert: Boolean = false
+    val count: Int
 )
