ff152c8ccc3a9383bd598d26daffbaf97f2d6eb5
seunghwan <lovelyunsh@naver.com>
2024-01-25 14:14:09 +0900

    Style : order status 프로세스 순서에 맞춰 변경

--- src/main/kotlin/com/teamsparta/backoffice/domain/order/model/OrderStatus.kt
+++ src/main/kotlin/com/teamsparta/backoffice/domain/order/model/OrderStatus.kt
@@ -3,8 +3,8 @@ package com.teamsparta.backoffice.domain.order.model
 enum class OrderStatus {
     CONFIRM_WAIT,
     COOKING,
-    DELIVERING,
-    FINISH,
     CUSTOMER_CANCEL,
-    STORE_CANCEL
+    STORE_CANCEL,
+    DELIVERING,
+    FINISH
 }
